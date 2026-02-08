0ac299cd829c114b3ee2118ed6d9e0ea8b211a2b
ItzMK <raomamoon592@gmail.com>
2026-02-08 16:19:30 +0500

    working

---
+++ app/databse/dbConnect.ts
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose");
+const connectToDb= async()=>{
+    try{
+   await mongoose.connect("mongodb+srv://mainUser:Mamoon592@cluster0.sgyfjmr.mongodb.net/CHECK?retryWrites=true&w=majority&appName=Cluster0", {
+   
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB is Connected");
+  }catch (err: unknown) {
+    const errorMessage = err instanceof Error ? err.message : String(err);
+    console.error("❌ DB Connection Failed:", errorMessage);
+}}
+module.exports = connectToDb;
