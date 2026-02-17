21189b5d70b0c2a1be1f665a30609c1e10efb460
ItzMK <raomamoon592@gmail.com>
2026-02-17 23:21:54 +0500

    feb17

---
+++ app/api/requwst/route.js
@@ -0,0 +1,41 @@
+import { NextResponse } from "next/server";
+import Contact from "../../model/Contact";
+import connectToDb from "@/app/database/dbConnect";
+
+export async function POST(req: Request) {
+  try {
+    await connectToDb();
+
+    const { name, mail, type,message } = await req.json();
+
+    // basic validation
+    if (!name || !mail || !type || !message) {
+      return NextResponse.json(
+        { error: "All fields are required" },
+        { status: 400 }
+      );
+    }
+
+    const newMessage = await Contact.create({
+      name,
+      mail,
+      type,
+      message,
+    });
+
+    return NextResponse.json(
+      {
+        success: true,
+        message: "Message saved successfully",
+        data: newMessage,
+      },
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error(error);
+    return NextResponse.json(
+      { error: "Failed to save message" },
+      { status: 500 }
+    );
+  }
+}
